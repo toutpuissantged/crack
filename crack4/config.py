34a973caf3ecb20f6fa97327d95398262f3a95f2
toutpuissantged <58160473+toutpuissantged@users.noreply.github.com>
2021-01-13 11:45:44 +0000

    Add files via upload

---
+++ crack4/config.py
@@ -0,0 +1,12 @@
+
+
+class env():
+
+    url='http://plk.com/login'
+    login='username'
+    psw='password'
+    longeur_du_code=4
+    nom_fichiers_login="login_found.txt"
+    invalid_password_error_msg="invalid password"
+
+
